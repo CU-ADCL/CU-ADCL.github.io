65f2b0c11ca5cf1e8e7e5b01732f8b0a9837ee0e
zlaouar <zlaouar@users.noreply.github.com>
2024-03-10 20:09:55 +0000

    deploy: b010ea44a5e6550f7e1180db93ed02455046938d

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
-const version = '20240304130740';
+const version = '20240310140949';
 const cacheName = `static::${version}`;
 
 const buildContentBlob = () => {
-  return ["/posts/compute/","/","/assets/css/main.css","/manifest.json","/people/","/posts/publication-checklist/","/publications/","/research/","/resources/","/assets/search.json","/assets/styles.css","/posts/writing/","/posts/yearly-checkin/","/assets/js/lunr/lunr-en.js","/assets/js/lunr/lunr-gr.js","/assets/js/lunr/lunr-store.js","/redirects.json","/sitemap.xml","/robots.txt","/feed.xml","/assets/logos/ADCL-rounded.svg", "/assets/default-offline-image.png", "/assets/scripts/fetch.js"
+  return ["/posts/compute/","/","/assets/css/main.css","/manifest.json","/people/","/posts/publication-checklist/","/publications/","/research/","/resources/","/assets/search.json","/assets/styles.css","/posts/writing/","/posts/yearly-checkin/","/assets/js/lunr/lunr-en.js","/assets/js/lunr/lunr-gr.js","/assets/js/lunr/lunr-store.js","/redirects.json","/pdfs/","/sitemap.xml","/robots.txt","/feed.xml","/assets/logos/ADCL-rounded.svg", "/assets/default-offline-image.png", "/assets/scripts/fetch.js"
   ]
 }
 
